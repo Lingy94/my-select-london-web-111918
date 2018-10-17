43cc8acdcb0e5dba64259a1b8881a5dbfa938553
Jack Ling <j.lingyx@gmail.com>
2018-10-17 15:17:45 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
-def my_select(collection)
- # your code here!
+def my_select(selection)
+i = 0
+newArray = []
+while i < selection.length
+    yield selection[i]
+   i += 1
+  end
+return newArray
+end
 end
