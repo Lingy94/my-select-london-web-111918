9c22e1f88ff042f2c19b2ea36fa19494618acd41
Jack Ling <j.lingyx@gmail.com>
2018-10-17 15:45:23 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(selection)
 i = 0
 newArray = []
 while i < selection.length
-  yield selection[i].true?
+  yield selection[i]
   newArray.push(selection[i]) #returns [1..5]
   i += 1
   end
